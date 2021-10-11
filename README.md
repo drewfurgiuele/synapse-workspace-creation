f0ef62a881dc28e5cbc184af1d7cea27336f9154
Drew Furgiuele <drew.furgiuele@microsoft.com>
2021-10-11 19:18:39 -0400

    updated readme and endpoints

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # synapse-workspace-creation
+
+This repository is set up to host automations scripts around the creation of Azure Synapse Analytics workspaces. Included in this codebase are two folders:
+
+* powershell-scripts: A PowerShell cmdlet-based script that creates a new Synapse Workspace, empty Dedicated pool (which is paused on creation), new "small" spark pool, and a private endpoint connection to both the workspace and workspace storage account (if requested)
+* arm-template: A default, bare-bones workspace deployment template which you can run through ```New-AzResourceGroupDeployment```
+
+## Requirements
+* Azure PowerShell Module
+* Az.Synapse Module
+* A current Azure context (```Connect-AzAccount```) in the subscription you want to deploy to (```Set-AzContext```)
--- powershell-scripts/New-SynapseWorkspace.ps1
+++ powershell-scripts/New-SynapseWorkspace.ps1
@@ -18,6 +18,7 @@ Param(
     [Parameter(Mandatory=$False)] [String] $SparkPoolNodeSize = "Small",
     [Parameter(Mandatory=$False)] [String] $SparkVersion = "3.1",
     [Parameter(Mandatory=$False)] [Hashtable] $Tags,
+    [Parameter(Mandatory=$False)] [Switch] $CreateWorkspacePrivateEndpoints,
     [Parameter(Mandatory=$False)] [Switch] $CreateStorageAccountPrivateEndpoint
 
 )
@@ -26,6 +27,12 @@ begin {
         $AzContext = Get-AzContext
     } catch {
         throw "Unable to fetch current Azure Context (Are you logged in via PowerShell?)"
+    }
+
+    try {
+        $RG = Get-AzResourceGroup -Name $ResourceGroupName
+    } catch {
+        throw "Unable to find resource group (are you in the right subscritpion?)"
     }
 
     if ($WorkspaceName -cnotmatch "^[^A-Z]*$" -or $WorkspaceName.Length -gt 16)
@@ -63,26 +70,30 @@ process {
     $ManagedVNetConfiguration = New-AzSynapseManagedVirtualNetworkConfig -PreventDataExfiltration
     $Workspace = New-AzSynapseWorkspace -ResourceGroupName $ResourceGroupName -Name $WorkspaceName -Location $Location -SqlAdministratorLoginCredential $SQLCredential -DefaultDataLakeStorageAccountName $DefaultStorageAccountName -DefaultDataLakeStorageFilesystem $DefaultStorageFileSystemName -ManagedVirtualNetwork $ManagedVNetConfiguration -ManagedResourceGroupName $WorkspaceManagedResourceGroupName
 
-    # Create dedicated pool
     Write-Verbose "Deploying empty SQL dedicated pool..." 
     $DedicatedPool = New-AzSynapseSqlPool -ResourceGroupName $ResourceGroupName -Name $SQLPoolName -WorkspaceName $WorkspaceName -PerformanceLevel $SqlPoolServiceTier
 
-    #Pause Pool
     Write-Verbose "Stopping recently-deployed SQL dedicated pool..."
     Suspend-AzSynapseSqlPool -ResourceGroupName $ResourceGroupName -WorkspaceName $WorkspaceName -Name $SQLPoolName
 
-    # Create spark pool
     Write-Verbose "Deploying Spark pool..." 
     $SparkPool = New-AzSynapseSparkPool -ResourceGroupName $ResourceGroupName -Name $SparkPoolName -WorkspaceName $WorkspaceName -SparkVersion $SparkVersion -AutoScaleMinNodeCount $SparkPoolMinNodes -AutoScaleMaxNodeCount $SparkPoolMaxNodes -EnableAutoPause -AutoPauseDelayInMinute 60 -NodeSize $SparkPoolNodeSize
 
-    # Create endpoints
-    Write-Verbose "Creating private endpoint for Workspace..."
 
-    $dev_privateEndpointConn = New-AzPrivateLinkServiceConnection -Name ($WorkspaceName + "_pls") -PrivateLinkServiceId $Workspace.Id -GroupId “dev”
-    $dev_privateEndpoint = New-AzPrivateEndpoint -ResourceGroupName $ResourceGroupName -Name ($WorkspaceName + "_pe") -Location $Location -Subnet $subnet -PrivateLinkServiceConnection $dev_privateEndpointConn
-    
+    if ($CreateWorkspacePrivateEndpoints) {
+        Write-Verbose "Creating private endpoint for Workspace..."
+        $dev_ws_privateEndpointConn = New-AzPrivateLinkServiceConnection -Name ($WorkspaceName + "_plsws") -PrivateLinkServiceId $Workspace.Id -GroupId “dev”
+        $dev_ws_privateEndpoint = New-AzPrivateEndpoint -ResourceGroupName $ResourceGroupName -Name ($WorkspaceName + "_pews") -Location $Location -Subnet $subnet -PrivateLinkServiceConnection $dev_ws_privateEndpointConn
+
+        $dev_sqlondemand_privateEndpointConn = New-AzPrivateLinkServiceConnection -Name ($WorkspaceName + "_plsondemand") -PrivateLinkServiceId $Workspace.Id -GroupId “sqlondemand”
+        $dev_sqlondemand_privateEndpoint = New-AzPrivateEndpoint -ResourceGroupName $ResourceGroupName -Name ($WorkspaceName + "_peondemand") -Location $Location -Subnet $subnet -PrivateLinkServiceConnection $dev_sqlondemand_privateEndpointConn
+
+        $dev_sql_privateEndpointConn = New-AzPrivateLinkServiceConnection -Name ($WorkspaceName + "_plssql") -PrivateLinkServiceId $Workspace.Id -GroupId “sql”
+        $dev_sql_privateEndpoint = New-AzPrivateEndpoint -ResourceGroupName $ResourceGroupName -Name ($WorkspaceName + "_pesql") -Location $Location -Subnet $subnet -PrivateLinkServiceConnection $dev_sql_privateEndpointConn
+
+    }
+
     if ($CreateStorageAccountPrivateEndpoint) {
-        # Storage endpoint
         Write-Verbose "Creating private endpoint for storage account..."    
         $storage_privateEndpointConn = New-AzPrivateLinkServiceConnection -Name ($WorkspaceName + "_storage_pls") -PrivateLinkServiceId $StorageAccount.Id -GroupId “dfs”
         $storage_privateEndpoint = New-AzPrivateEndpoint -ResourceGroupName $ResourceGroupName -Name ($WorkspaceName + "storage_pe") -Location $Location -Subnet $subnet -PrivateLinkServiceConnection $storage_privateEndpointConn
