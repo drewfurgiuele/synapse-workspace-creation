b21448bc559b3009eb9f52536ec9d598df11370e
Drew Furgiuele <drew.furgiuele@microsoft.com>
2021-10-11 15:11:26 -0400

    initial checkin

---
+++ powershell-scripts/New-SynapseWorkspace.ps1
@@ -0,0 +1,90 @@
+#Requires -Modules Az.Synapse
+[cmdletbinding()]
+Param(
+    [Parameter(Mandatory=$True)] [String] $ResourceGroupName,
+    [Parameter(Mandatory=$True)] [String] $Location,
+    [Parameter(Mandatory=$True)] [String] $WorkspaceName,
+    [Parameter(Mandatory=$True)] [String] $WorkspaceManagedResourceGroupName,    
+    [Parameter(Mandatory=$True)] [String] $DefaultStorageAccountName,
+    [Parameter(Mandatory=$True)] [String] $DefaultStorageFileSystemName,    
+    [Parameter(Mandatory=$True)] [String] $PrivateEndpointVNetName,
+    [Parameter(Mandatory=$True)] [String] $PrivateEndpointSubnetName,    
+    [Parameter(Mandatory=$True)] [String] $SQLPoolName,
+    [Parameter(Mandatory=$True)] [pscredential] $SQLCredential,
+    [Parameter(Mandatory=$False)] [String] $SqlPoolServiceTier = "DW100c",
+    [Parameter(Mandatory=$True)] [String] $SparkPoolName,
+    [Parameter(Mandatory=$False)] [int] $SparkPoolMinNodes = 3,
+    [Parameter(Mandatory=$False)] [int] $SparkPoolMaxNodes = 3,
+    [Parameter(Mandatory=$False)] [String] $SparkPoolNodeSize = "Small",
+    [Parameter(Mandatory=$False)] [String] $SparkVersion = "3.1",
+    [Parameter(Mandatory=$False)] [Hashtable] $Tags,
+    [Parameter(Mandatory=$False)] [Switch] $CreateStorageAccountPrivateEndpoint
+
+)
+begin {
+    try {
+        $AzContext = Get-AzContext
+    } catch {
+        throw "Unable to fetch current Azure Context (Are you logged in via PowerShell?)"
+    }
+
+    if ($WorkspaceName -cnotmatch "^[^A-Z]*$" -or $WorkspaceName.Length -gt 16)
+    {
+        throw "Workspace name must be all lower case and less than 15 characters"
+    }
+    if ($DefaultStorageAccountName -cnotmatch "^[^A-Z]*$" -or $DefaultStorageAccountName.Length -gt 25)
+    {
+        throw "The workspace default storage account name must be all lower case and less than 25 characters"
+    }
+    if ($SQLPoolName -cnotmatch "^[^A-Z]*$" -or $SQLPoolName.Length -gt 16)
+    {
+        throw "The dedicated SQL pool name must be all lower case and less than 15 characters"
+    }
+    if ($SparkPoolName -cnotmatch "^[^A-Z]*$" -or $SparkPoolName.Length -gt 16)
+    {
+        throw "The Spark pool name Workspace name must be all lower case and less than 15 characters"
+    }
+
+    #Verify Storage account and container exist
+    $StorageAccount = Get-AzStorageAccount -Name $DefaultStorageAccountName -ResourceGroupName $ResourceGroupName
+
+    #Verify network ans subnet
+    try {
+        $vnet = Get-AzVirtualNetwork -Name $PrivateEndpointVNetName -ResourceGroupName $ResourceGroupName
+        $subnet = $vnet.Subnets | where-object {$_.name -eq $PrivateEndpointSubnetName}
+    } catch {
+        throw "Unable to validate target private endpoint virtual network/subnet configuration. Did you supply the correct VNet and subnet name?"
+    }
+}
+
+process {
+    Write-Verbose "Deploying workspace..."
+    # Create workspace
+    $ManagedVNetConfiguration = New-AzSynapseManagedVirtualNetworkConfig -PreventDataExfiltration
+    $Workspace = New-AzSynapseWorkspace -ResourceGroupName $ResourceGroupName -Name $WorkspaceName -Location $Location -SqlAdministratorLoginCredential $SQLCredential -DefaultDataLakeStorageAccountName $DefaultStorageAccountName -DefaultDataLakeStorageFilesystem $DefaultStorageFileSystemName -ManagedVirtualNetwork $ManagedVNetConfiguration -ManagedResourceGroupName $WorkspaceManagedResourceGroupName
+
+    # Create dedicated pool
+    Write-Verbose "Deploying empty SQL dedicated pool..." 
+    $DedicatedPool = New-AzSynapseSqlPool -ResourceGroupName $ResourceGroupName -Name $SQLPoolName -WorkspaceName $WorkspaceName -PerformanceLevel $SqlPoolServiceTier
+
+    #Pause Pool
+    Write-Verbose "Stopping recently-deployed SQL dedicated pool..."
+    Suspend-AzSynapseSqlPool -ResourceGroupName $ResourceGroupName -WorkspaceName $WorkspaceName -Name $SQLPoolName
+
+    # Create spark pool
+    Write-Verbose "Deploying Spark pool..." 
+    $SparkPool = New-AzSynapseSparkPool -ResourceGroupName $ResourceGroupName -Name $SparkPoolName -WorkspaceName $WorkspaceName -SparkVersion $SparkVersion -AutoScaleMinNodeCount $SparkPoolMinNodes -AutoScaleMaxNodeCount $SparkPoolMaxNodes -EnableAutoPause -AutoPauseDelayInMinute 60 -NodeSize $SparkPoolNodeSize
+
+    # Create endpoints
+    Write-Verbose "Creating private endpoint for Workspace..."
+
+    $dev_privateEndpointConn = New-AzPrivateLinkServiceConnection -Name ($WorkspaceName + "_pls") -PrivateLinkServiceId $Workspace.Id -GroupId “dev”
+    $dev_privateEndpoint = New-AzPrivateEndpoint -ResourceGroupName $ResourceGroupName -Name ($WorkspaceName + "_pe") -Location $Location -Subnet $subnet -PrivateLinkServiceConnection $dev_privateEndpointConn
+    
+    if ($CreateStorageAccountPrivateEndpoint) {
+        # Storage endpoint
+        Write-Verbose "Creating private endpoint for storage account..."    
+        $storage_privateEndpointConn = New-AzPrivateLinkServiceConnection -Name ($WorkspaceName + "_storage_pls") -PrivateLinkServiceId $StorageAccount.Id -GroupId “dfs”
+        $storage_privateEndpoint = New-AzPrivateEndpoint -ResourceGroupName $ResourceGroupName -Name ($WorkspaceName + "storage_pe") -Location $Location -Subnet $subnet -PrivateLinkServiceConnection $storage_privateEndpointConn
+    }
+}
